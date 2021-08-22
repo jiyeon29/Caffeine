20471fb4e42f2690b10a309e0acbfd679af59915
jiyeon29 <cjy3094@naver.com>
2021-08-23 02:04:27 +0900

    MyInfo

--- Caffeine/Caffeine/Controller/MyInfoViewController.swift
+++ Caffeine/Caffeine/Controller/MyInfoViewController.swift
@@ -8,28 +8,80 @@
 import UIKit
 
 class MyInfoViewController: UIViewController {
-
+    
     @IBOutlet weak var tfName: UITextField!
+    @IBOutlet weak var tfAge: UITextField!
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var switchPregnancy: UISwitch!
+
+    // 전역변수 선언
+    let myUserDefaults = UserDefaults.standard
+   // var user
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        if let userName = UserDefaults.standard.string(forKey: "userName"){
+    /*
+     let defaults = UserDefaults.standard
+     userInput = defaults.string(forKey: "userInput")!
+
+     */
+    
+    // 0,1 (int)
+    
+   override func viewWillAppear(_ animated: Bool) {
+    
+   // let userName = ""
+    
+
+    if let userName:String = myUserDefaults.string(forKey: "userName") {
+           let userAge:Int = myUserDefaults.integer(forKey: "userAge")
+           let userWeight:Int = myUserDefaults.integer(forKey: "userWeight")
+           let userPregnancy:Bool = myUserDefaults.bool(forKey: "userPregnancy")
+
             tfName.text = userName
-        }else{
-            self.navigationItem.setHidesBackButton(true, animated: false)
-        }
-    }
+            tfAge.text = String(userAge)
+            tfWeight.text = String(userWeight)
+            switchPregnancy.isOn = userPregnancy
+      }else{
+
+        self.navigationItem.setHidesBackButton(true, animated: false)
+
+      }
+
+   }
+    
+    // 넘버패드로 바꾸기
     
     @IBAction func btnRegister(_ sender: UIButton) {
-        guard let userName : String = tfName.text?.trimmingCharacters(in: .whitespacesAndNewlines) else{ return }
         
-        UserDefaults.standard.set(userName, forKey: "userName")
-        self.navigationController?.popViewController(animated: true)
-    }
+        guard let userName:String = tfName.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
+            return
+        }
+        
+        guard let userAge:Int = Int(tfAge.text!)! as Int? else {
+            return
+        }
+        
+        guard let userWeight : Int = Int(tfWeight.text!)! as Int? else {
+            return
+        }
+        
+        guard let userPregnancy : Bool = switchPregnancy.isOn as Bool? else {
+            return
+        }
+
+        myUserDefaults.set(userName, forKey: "userName")
+        myUserDefaults.set(userAge, forKey: "userAge")
+        myUserDefaults.set(userWeight, forKey: "userWeight")
+        myUserDefaults.set(userPregnancy, forKey: "userPregnancy")
+
+        self.navigationController?.popViewController(animated: true) // 현재 화면 사라짐
+        
+    } // ---------- btnRegister
     
 
     /*
@@ -42,4 +94,4 @@ class MyInfoViewController: UIViewController {
     }
     */
 
-}
+} // ---------- MyInfoViewController
